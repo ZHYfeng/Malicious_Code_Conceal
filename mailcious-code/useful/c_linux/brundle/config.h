4980e8c2f35d81561a9a677cb62b1b6b92617313
zhy <haoyu.zz@foxmail.com>
2016-10-09 13:05:25 +0800

    change name

---
+++ mailcious-code/useful/c_linux/brundle/config.h
@@ -0,0 +1,21 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if you have the <elf.h> header file.  */
+#define HAVE_ELF_H 1
+
+/* Define if you have the <sys/elf.h> header file.  */
+/* #undef HAVE_SYS_ELF_H */
+
+/* Define if you have the <sys/exec_elf.h> header file.  */
+/* #undef HAVE_SYS_EXEC_ELF_H */
+
+/* Name of package */
+#define PACKAGE "brundle-fly"
+
+/* Version number of package */
+#define VERSION "0.0.1"
+
