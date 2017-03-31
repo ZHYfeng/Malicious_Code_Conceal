7aa846d0894e271cea690d3893ef8441234861fc
HY <haoyu.zz@foxmail.com>
2017-03-31 16:55:38 +0800

    finish

--- 3_2_multi_programmes/cholesky.4321/script/run.bat
+++ 3_2_multi_programmes/cholesky.4321/script/run.bat
@@ -4,28 +4,32 @@ set RUN_TIMES=10000
 del %current_path%\statistic.log
 
 for /l %%b in (1, 1, %RUN_TIMES%) do (
-    %current_path%\..\programme\programme.exe %current_path%\..\programme\inputs\d750.O
+    %current_path%\..\programme\programme.exe
 )
 move %current_path%\statistic.log %current_path%\..\statistic\0_load.log
+move %current_path%\sequence.log %current_path%\..\statistic\sequence_0_load.log
 
 start  %current_path%\..\..\..\2_base\0_payload\programme\payload.exe
 for /l %%b in (1, 1, %RUN_TIMES%) do (
-    %current_path%\..\programme\programme.exe %current_path%\..\programme\inputs\d750.O
+    %current_path%\..\programme\programme.exe
 )
 move %current_path%\statistic.log %current_path%\..\statistic\1_load.log
+move %current_path%\sequence.log %current_path%\..\statistic\sequence_1_load.log
 
 for /l %%x in (0, 1, 6) do (
     start  %current_path%\..\..\..\2_base\0_payload\programme\payload.exe
 )
 for /l %%b in (1, 1, %RUN_TIMES%) do (
-    %current_path%\..\programme\programme.exe %current_path%\..\programme\inputs\d750.O
+    %current_path%\..\programme\programme.exe
 )
 move %current_path%\statistic.log %current_path%\..\statistic\7_load.log
+move %current_path%\sequence.log %current_path%\..\statistic\sequence_7_load.log
 
 start  ..\..\..\2_base\0_payload\programme\payload.exe
 for /l %%b in (1, 1, %RUN_TIMES%) do (
-    %current_path%\..\programme\programme.exe %current_path%\..\programme\inputs\d750.O
+    %current_path%\..\programme\programme.exe
 )
 move %current_path%\statistic.log %current_path%\..\statistic\8_load.log
+move %current_path%\sequence.log %current_path%\..\statistic\sequence_8_load.log
 
 taskkill /IM payload.exe
