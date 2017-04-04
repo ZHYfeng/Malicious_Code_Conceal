03d9763fa4e53f1315500e3dd1e2ae8acb3ecd6d
HY <haoyu.zz@foxmail.com>
2017-04-04 12:38:57 +0800

    ...

--- 3_2_multi_programmes/run.3.bat
+++ 3_2_multi_programmes/run.3.bat
@@ -1,19 +1,21 @@
 set current_path=%cd%
 set RUN_TIMES=1000
+set sleep_time=2000
 
 for /d %%i in (*) do (
 
     cd %%i/programme
-    del *.o *.exe 
-    del statistic.log sequence.log
+    REM del *.o *.exe 
+    REM del statistic.log sequence.log
     REM make
 
     cd ../script
-    del statistic.log sequence.log
+    REM del statistic.log sequence.log
     copy /y ..\..\run.2.bat .\run.bat
     REM copy /y ..\..\run.1.bat .\run.bat
     REM run.bat
-
+    start  ..\..\..\2_base\0_payload\programme\payload.exe
+    sleep(%sleep_time%)
     taskkill /IM payload.exe
     cd ../statistic
     
