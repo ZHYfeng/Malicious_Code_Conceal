6fab23dffb139eabbf98dece402331691bd4fa1c
unknown <haoyu.zz@foxmail.com>
2017-04-19 17:09:43 +0800

    add occurrence py

--- 0_statistic/statistic_sequence.sh
+++ 0_statistic/statistic_sequence.sh
@@ -5,12 +5,10 @@ do
     python3 ../../0_statistic/calc_subsec_and_sort.py -f $FILE -s 1-2-3-4 
 done
 
-rm result.txt
+rm sequence_result.txt
 for FILE in ./sequence_*.log.1-2-3-4
 do 
-    echo -n $FILE >> result.txt
-    echo -n ", " >> result.txt
-    cat $FILE >> result.txt
+    echo -n $FILE >> sequence_result.txt
+    echo -n ", " >> sequence_result.txt
+    cat $FILE >> sequence_result.txt
 done
-
-
--- 3_windows_load.pthread/script/run_payload.bat
+++ 3_windows_load.pthread/script/run_payload.bat
@@ -1,26 +1,26 @@
 
-set RUN_TIMES=10
+set RUN_TIMES=10000
 set THREADS=4
 set MALICIOUS=4
 set LOOPS=40000
 
+call:run_load 30000
+call:run_load 40000
+call:run_load 60000
+call:run_load 120000
+
 :run_load
 set LOOPS=%1
-del statistic.log sequence.log
+del occurrence.log sequence.log
 for /l %%x in (0, 1, 8) do (
     for /l %%b in (1, 1, %RUN_TIMES%) do (
         ..\programme\programme.exe %THREADS% %MALICIOUS% %LOOPS%
     )
     move occurrence.log ..\statistic\occurrence_load_%%x_loop_%LOOPS%.log
     move sequence.log ..\statistic\sequence_load_%%x_loop_%LOOPS%.log
-    start /b ..\..\0_payload\programme\bin\Debug\payload.exe
+    start /b ..\..\0_payload\programme\payload.exe
 )
 taskkill /F /IM payload.exe
 GOTO:EOF
-
-call:run_load 30000
-call:run_load 40000
-call:run_load 60000
-call:run_load 120000
 
 exit
