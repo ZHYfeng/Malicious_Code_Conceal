cd3da4beab692755f547a667c6b5c9027357b3ae
unknown <haoyu.zz@foxmail.com>
2017-08-12 18:02:35 +0800

    new remote

---
+++ 5_remot/get_cpu_usage1.ps1
@@ -0,0 +1,22 @@
+﻿$logfile = 'log.txt'
+Start-Transcript $logfile -Append -Force
+$counters=new-object 'System.Diagnostics.PerformanceCounter[]' ([System.Environment]::ProcessorCount)
+for($i=0;$i -lt $counters.Length;$i++){
+    $counters[$i]=new-object System.Diagnostics.PerformanceCounter("Processor", "% Processor Time",$i)
+}
+for($j=0; $j -lt 10; $j++){
+    Get-Date
+    foreach($c in $counters){
+        $cpu =  $c.NextValue();
+        $cpu;
+
+    }
+        if($cpu -ge 0){
+            for($i=0; $i -lt 1; $i++){
+                Start-Process -FilePath .\programme.exe
+            }
+        }
+    echo ---------
+    sleep 1
+}
+Stop-Transcript
--- 5_remot/run_all.bat
+++ 5_remot/run_all.bat
@@ -1,4 +1,4 @@
-set RUN_TIMES=50
+set RUN_TIMES=10
 
         for /l %%b in (1, 1, %RUN_TIMES%) do (
             .\run.bat
