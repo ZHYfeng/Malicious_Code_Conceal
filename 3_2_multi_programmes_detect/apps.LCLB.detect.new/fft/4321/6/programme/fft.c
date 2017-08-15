204b0b5185a91ab371ef186b70a78419da3708be
HY <1129719536@qq.com>
2017-08-15 18:59:35 +0800

    fft

--- 3_2_multi_programmes_detect/apps.LCLB.detect.new/fft/4321/6/programme/fft.c
+++ 3_2_multi_programmes_detect/apps.LCLB.detect.new/fft/4321/6/programme/fft.c
@@ -630,10 +630,7 @@ void SlaveStart()
 #line 440
   };
 
-malicious_4();
-malicious_3();
-malicious_2();
-malicious_1();
+
 
   upriv = (double *)malloc(2 * (rootN - 1) * sizeof(double));
   if (upriv == NULL)
@@ -932,7 +929,10 @@ void FFT1D(long direction, long M, long N, double *x, double *scratch, double *u
     pthread_barrier_wait(&(Global->start));
 #line 643
   };
-
+  malicious_4();
+  malicious_3();
+  malicious_2();
+  malicious_1();
   if ((MyNum == 0) || (dostats))
   {
     {
