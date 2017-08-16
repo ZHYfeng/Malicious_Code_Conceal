d0b17b49ef4c787d20358404b8b784427b6b2dde
HY <1129719536@qq.com>
2017-08-16 14:02:20 +0800

    ..

--- 3_2_multi_programmes_detect/script/modify_makefile.py
+++ 3_2_multi_programmes_detect/script/modify_makefile.py
@@ -34,18 +34,19 @@ for parent, dirnames, filenames in os.walk(detectdir):
     for filename in filenames:
         if "makefile" == filename:
             # replace makefile
-            print(os.path.join(parent, filename))
-            infile = open(os.path.join(parent, filename), "r")
-            outfile = open(os.path.join(parent, "temp_makefile"), "w")
-            lines = infile.readlines()
-            for line in lines:
-                if line.startswith("include"):
-                    outfile.write(
-                        "include ..\\..\\..\\..\\..\\null_macros\\Makefile.config\n")
-                else:
-                    outfile.write(line)
-            infile.close()
-            outfile.close()
-            os.remove(os.path.join(parent, filename))
-            shutil.move(os.path.join(parent, "temp_makefile"),
-                        os.path.join(parent, filename))
+            # print(os.path.join(parent, filename))
+            # infile = open(os.path.join(parent, filename), "r")
+            # outfile = open(os.path.join(parent, "temp_makefile"), "w")
+            # lines = infile.readlines()
+            # for line in lines:
+            #     if line.startswith("include"):
+            #         outfile.write(
+            #             "include ..\\..\\..\\..\\..\\null_macros\\Makefile.config\n")
+            #     else:
+            #         outfile.write(line)
+            # infile.close()
+            # outfile.close()
+            # os.remove(os.path.join(parent, filename))
+            # shutil.move(os.path.join(parent, "temp_makefile"),
+            #             os.path.join(parent, filename))
+            os.system('make')  
